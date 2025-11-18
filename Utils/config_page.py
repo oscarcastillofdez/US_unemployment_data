2a9d1ffb40d963c37b94af480c62a98850100516
oscarcastillofdez <oscarcastillofdez@gmail.com>
2025-11-18 13:25:05 +0100

    Update config_page.py

--- Utils/config_page.py
+++ Utils/config_page.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 def config():
     st.sidebar.page_link('app.py', label='Summary')
-    st.sidebar.page_link('state_analysis.py', label='State Analysis')
-    st.sidebar.page_link('prediction.py', label='Prediction')
-    st.sidebar.page_link('typical_year.py', label='Average year')
+    st.sidebar.page_link('pages/state_analysis.py', label='State Analysis')
+    st.sidebar.page_link('pages/prediction.py', label='Prediction')
+    st.sidebar.page_link('pages/typical_year.py', label='Average year')
     if st.session_state.admin or st.session_state.dev:
-        st.sidebar.page_link('Pages/data.py', label='Data')
+        st.sidebar.page_link('pages/data.py', label='Data')
     if st.session_state.admin:
-        st.sidebar.page_link('Pages/users.py', label='Users')
+        st.sidebar.page_link('pages/users.py', label='Users')
 
     st.set_page_config(
         page_title="USA Employment",
